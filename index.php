aed9e3d5c7e006f6d2cc12e039b6a9dca3ba6b90
IvanReshetnikovGit <ivanreshet@icloud.com>
2025-06-02 20:01:34 +0300

    com

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
                     <td><input type="text" name="group" value="<?php if(isset($_POST['group'])) echo $_POST['group']; ?>"></td>
                 </tr>
                 <tr>
+                    <td>x:</td>
+                    <td><input type="text" name="x" value="<?php if(isset($_POST['x'])) echo $_POST['x']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>y:</td>
+                    <td><input type="text" name="y" value="<?php if(isset($_POST['y'])) echo $_POST['y']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>z:</td>
+                    <td><input type="text" name="z" value="<?php if(isset($_POST['z'])) echo $_POST['z']; ?>"></td>
+                </tr>
+                <tr>
                     <td colspan="2"><hr></td>
                 </tr>
                     <td><input type="hidden" name="confirm" value="1"></td>
@@ -31,7 +43,7 @@
     </body>
     <?php
 function calculateFunction($x, $y, $z) {
-    return (2 * cos(pow($x, 0.1) - pi() / 6)) / (($y / $x) + pow(sin(pow($y, 0.3)), 2)) + pow(log($z), 2 * 0.3);
+    return ($x+$y+$z);
 }
 
 if (isset($_POST['confirm'])) {
@@ -39,21 +51,23 @@ if (isset($_POST['confirm'])) {
     $lastname = $_POST['lastname'];
     $firstname = $_POST['firstname'];
     $group = $_POST['group'];
+    $x = $_POST['x'];
+    $y = $_POST['y'];
+    $z = $_POST['z'];
     $variant = 9;
     
     if (empty($lastname)){ $error .= "Не введено прізвище<br>";}
     if (empty($firstname)){ $error .= "Не введено ім'я<br>";}
     if (empty($group)){ $error .= "Не введено групу<br>";}
-    if (empty($variant)){ $error .= "Не введено номер варіанту<br>";}
+    if (empty($x)){ $error .= "Не введено x<br>";}
+    if (empty($y)){ $error .= "Не введено y<br>";}
+    if (empty($z)){ $error .= "Не введено z<br>";}
     
     if ($error != "Помилка: ") {
         echo $error;
     } else {
-        $x1 = 1.23 * $variant;
-        $y1 = 4.6 * $variant;
-        $z1 = 36.6 / $variant;
         
-        $result = calculateFunction($x1, $y1, $z1);
+        $result = calculateFunction($x, $y, $z);
         
         echo "<h3>Результати:</h3>";
         echo "<table border='1'>";
@@ -61,9 +75,9 @@ if (isset($_POST['confirm'])) {
         echo "<tr><td>Ім'я:</td><td>$firstname</td></tr>";
         echo "<tr><td>Група:</td><td>$group</td></tr>";
         echo "<tr><td>Номер варіанту:</td><td>$variant</td></tr>";
-        echo "<tr><td>X1:</td><td>$x1</td></tr>";
-        echo "<tr><td>Y1:</td><td>$y1</td></tr>";
-        echo "<tr><td>Z1:</td><td>$z1</td></tr>";
+        echo "<tr><td>X1:</td><td>$x</td></tr>";
+        echo "<tr><td>Y1:</td><td>$y</td></tr>";
+        echo "<tr><td>Z1:</td><td>$z</td></tr>";
         echo "<tr><td>Результат функції:</td><td>$result</td></tr>";
         echo "</table>";
         
@@ -76,20 +90,15 @@ if (isset($_POST['confirm'])) {
             Номер варіанту: $variant
             
             Вхідні дані:
-            X1 = $x1
-            Y1 = $y1
-            Z1 = $z1
+            X1 = $x
+            Y1 = $y
+            Z1 = $z
             
             Результат обчислення функції: $result
         ";
         $headers = "From: naruto.ivanr@gmail.com\r\nReply-To: naruto.ivanr@gmail.com\r\nX-Mailer: PHP/" . phpversion();
 
         if (!mail($to, $subject, $message, $headers)) {
-            error_log("Не вдалося надіслати email користувачу $to");
-            echo "<p>Помилка при відправці email. Перевірте лог сервера або налаштування PHP.</p>";
-}
-
-        else {
             echo "<p>Помилка при відправці email.</p>";
         }
     }
